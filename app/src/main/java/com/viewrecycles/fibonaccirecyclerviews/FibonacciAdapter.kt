9cd6a4c0699fa895c32b44772a2c4910f8885ece
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2023-05-25 17:00:40 +0300

    made changes

--- app/src/main/java/com/viewrecycles/fibonaccirecyclerviews/FibonacciAdapter.kt
+++ app/src/main/java/com/viewrecycles/fibonaccirecyclerviews/FibonacciAdapter.kt
@@ -6,8 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-//class FibonacciAdapter {
-//}
+
 class FibonacciAdapter( val fibonacciNumbers: List<Long>) :
     RecyclerView.Adapter<FibonacciAdapter.FibonacciViewHolder>() {
 
